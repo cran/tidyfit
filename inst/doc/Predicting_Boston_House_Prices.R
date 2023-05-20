ccfe05d5a608317fa7f4ef496364eee7a58b0c41
Johann Pfitzinger <johann.pfitzinger@gmail.com>
2023-05-20 14:40:02 +0000

    version 0.6.4

--- inst/doc/Predicting_Boston_House_Prices.R
+++ inst/doc/Predicting_Boston_House_Prices.R
@@ -87,11 +87,11 @@ model_frame %>%
 ## ---- echo = F, eval=TRUE-----------------------------------------------------
 library(kableExtra)
 tbl <- data.frame(
-  `Best subset` = c("Exhaustive search (`leaps`)", "Forward selection (`leaps`)", "Backward selection (`leaps`)", "Sequential Replacement (`leaps`)"),
-  `L1/L2 penalized` = c("Lasso (`glmnet`)", "Ridge (`glmnet`)", "ElasticNet (`glmnet`)", "Adaptive Lasso (`glmnet`)"),
-  `Latent factors` = c("Principal components regression (`pls`)", "Partial least squares (`pls`)", "", ""),
-  `Machine Learning` = c("Gradient Boosting (`mboost`)", "Hierarchical feature regression (`hfr`)", "Support vector regression (`e1071`)", ""),
-  `Bayesian` = c("Bayesian regression (`arm`)", "Bayesian model averaging (`BMS`)", "Bayesian Lasso (`monomvn`)", "Bayesian Ridge (`monomvn`)"),
+  `Best subset` = c("Exhaustive search (`leaps`)", "Forward selection (`leaps`)", "Backward selection (`leaps`)", "Sequential Replacement (`leaps`)", "Genetic Algorithm ('gaselect')"),
+  `L1/L2 penalized` = c("Lasso (`glmnet`)", "Ridge (`glmnet`)", "ElasticNet (`glmnet`)", "Adaptive Lasso (`glmnet`)", ""),
+  `Latent factors` = c("Principal components regression (`pls`)", "Partial least squares (`pls`)", "", "", ""),
+  `Machine Learning` = c("Gradient Boosting (`mboost`)", "Hierarchical feature regression (`hfr`)", "Support vector regression (`e1071`)", "", ""),
+  `Bayesian` = c("Bayesian regression (`arm`)", "Bayesian model averaging (`BMS`)", "Bayesian Lasso (`monomvn`)", "Bayesian Ridge (`monomvn`)", "Spike and Slab Regression ('BoomSpikeSlab')"),
   check.names = F
 )
 kbl(tbl, align = "ccccc") %>% 
@@ -103,12 +103,13 @@ kbl(tbl, align = "ccccc") %>%
 #    regress(medv ~ .*.,
 #            OLS = m("lm"),
 #            BAYES = m("bayes"),
-#            BMA = m("bma", iter = 5000),
+#            BMA = m("bma", iter = 10000),
 #            SEQREP = m("subset", method = "seqrep", IC = "AIC"),
 #            `FORWARD SELECTION` = m("subset", method = "forward", IC = "AIC"),
 #            `BACKWARD SELECTION` = m("subset", method = "backward", IC = "AIC"),
 #            LASSO = m("lasso"),
 #            BLASSO = m("blasso"),
+#            SPIKESLAB = m("spikeslab", niter = 10000),
 #            RIDGE = m("ridge"),
 #            BRIDGE = m("bridge"),
 #            ELASTICNET = m("enet"),
@@ -118,6 +119,7 @@ kbl(tbl, align = "ccccc") %>%
 #            HFR = m("hfr"),
 #            `GRADIENT BOOSTING` = m("boost"),
 #            SVR = m("svm"),
+#            GENETIC = m("genetic", populationSize = 1000, numGenerations = 50, statistic = "AIC", maxVariables = 20),
 #            .cv = "vfold_cv", .cv_args = list(v = 10))
 
 ## -----------------------------------------------------------------------------
